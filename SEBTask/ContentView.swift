fdc835bc9aeff3e7c47a25db1c3a21ddffbe1bf3
ViliusBundulas <vilius.bundulas@gmail.com>
2021-08-18 00:59:47 +0300

    Initial Commit

---
+++ SEBTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SEBTask
+//
+//  Created by Vilius Bundulas on 2021-08-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

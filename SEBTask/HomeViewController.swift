d403e526ab2c94a29a717cf4ff02b3177b0c40b7
ViliusBundulas <vilius.bundulas@gmail.com>
2021-08-18 01:05:21 +0300

    Setup project for programatic UI

---
+++ SEBTask/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  SEBTask
+//
+//  Created by Vilius Bundulas on 2021-08-18.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+    }
+}

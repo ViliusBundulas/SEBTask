230e4891db4d626690579faed17cf87b6010e601
ViliusBundulas <vilius.bundulas@gmail.com>
2021-08-20 18:53:16 +0300

    Update ApiManager.swift

--- Networking/ApiManager.swift
+++ Networking/ApiManager.swift
@@ -13,7 +13,6 @@ class ApiManager {
     private let sourcesURL = "https://sheet.best/api/sheets/ebb5bfdc-efda-4966-9ecf-d2c171d6985a"
     
     func fetchTransactions(completion: @escaping (AFResult<Transactions>) -> Void) {
-        
         AF.request(sourcesURL).responseDecodable { (response: AFDataResponse<Transactions>) in
             completion(response.result)
         }

bc4bc292e2aff642faaaf661c28155e0dec495e5
ViliusBundulas <vilius.bundulas@gmail.com>
2021-08-23 10:07:26 +0300

    Added validate() method
    Validate() method checks if status code is 2xx and optionally provided Accept part of the header matches the response's Content-Type

--- Networking/ApiManager.swift
+++ Networking/ApiManager.swift
@@ -13,7 +13,7 @@ class ApiManager {
     private let sourcesURL = "https://sheet.best/api/sheets/ebb5bfdc-efda-4966-9ecf-d2c171d6985a"
     
     func fetchTransactions(completion: @escaping (AFResult<Transactions>) -> Void) {
-        AF.request(sourcesURL).responseDecodable { (response: AFDataResponse<Transactions>) in
+        AF.request(sourcesURL).validate().responseDecodable { (response: AFDataResponse<Transactions>) in
             completion(response.result)
         }
     }
